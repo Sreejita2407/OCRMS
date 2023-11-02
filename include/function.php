81c92b77751df8313f8f0e8986b38c9fcea73f7a
Sreejita Mukhopadhyay <sreejita.tmsl@gmail.com>
2023-11-02 15:54:50 +0530

    a little mistake :)

--- include/function.php
+++ include/function.php
@@ -34,16 +34,6 @@
 		return strftime("%B %d, %Y at %I:%M %p", $nicetime);	
 					
 	}
-	function myAutoloader($class_name) {
-		$class_name = strtolower($class_name);
-		$path = LIB_PATH.DS."{$class_name}.php";
-		if(file_exists($path)){
-			require_once($path);
-		}else{
-			die("The file {$class_name}.php could not be found.");
-		}
-					
-	}
 	
 	function myAutoloader($class_name) {
     	$class_name = strtolower($class_name);
