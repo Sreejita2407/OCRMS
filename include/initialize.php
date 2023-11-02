f77e7930dd7dd3c4dc8b7bec5c0365deacd93f77
Sreejita Mukhopadhyay <sreejita.tmsl@gmail.com>
2023-11-02 15:45:19 +0530

    changed all the errors :) the college pic and name has to be changed

--- include/initialize.php
+++ include/initialize.php
@@ -11,30 +11,30 @@ defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.
 defined('LIB_PATH') ? null : define ('LIB_PATH',SITE_ROOT.DS.'include');
 
 //load the database configuration first.
-require_once(LIB_PATH.DS."config.php");
-require_once(LIB_PATH.DS."function.php");
-require_once(LIB_PATH.DS."session.php");
-require_once(LIB_PATH.DS."accounts.php");
-require_once(LIB_PATH.DS."autonumbers.php");
-require_once(LIB_PATH.DS."departments.php");
-require_once(LIB_PATH.DS."courses.php");
-require_once(LIB_PATH.DS."subjects.php");
-require_once(LIB_PATH.DS."sidebarFunction.php"); 
-require_once(LIB_PATH.DS."instructors.php");
-require_once(LIB_PATH.DS."schedules.php");
-require_once(LIB_PATH.DS."students.php");
-require_once(LIB_PATH.DS."classes.php");
-require_once(LIB_PATH.DS."studentsubjects.php");
-require_once(LIB_PATH.DS."studentschedule.php");
+require_once('C:/xampp/htdocs/project/include/config.php');
+require_once('C:/xampp/htdocs/project/include/function.php');
+require_once('C:/xampp/htdocs/project/include/session.php');
+require_once('C:/xampp/htdocs/project/include/accounts.php');
+require_once('C:/xampp/htdocs/project/include/autonumbers.php');
+require_once('C:/xampp/htdocs/project/include/departments.php');
+require_once('C:/xampp/htdocs/project/include/courses.php');
+require_once('C:/xampp/htdocs/project/include/subjects.php');
+require_once('C:/xampp/htdocs/project/include/sidebarFunction.php'); 
+require_once('C:/xampp/htdocs/project/include/instructors.php');
+require_once('C:/xampp/htdocs/project/include/schedules.php');
+require_once('C:/xampp/htdocs/project/include/students.php');
+require_once('C:/xampp/htdocs/project/include/classes.php');
+require_once('C:/xampp/htdocs/project/include/studentsubjects.php');
+require_once('C:/xampp/htdocs/project/include/studentschedule.php');
 // require_once(LIB_PATH.DS."grades.php");
-require_once(LIB_PATH.DS."semester.php"); 
-require_once(LIB_PATH.DS."studentdetails.php");
-require_once(LIB_PATH.DS."ay.php");
-require_once(LIB_PATH.DS."exam.php");
-require_once(LIB_PATH.DS."percent.php");
-require_once(LIB_PATH.DS."grade.php");
+require_once('C:/xampp/htdocs/project/include/semester.php'); 
+require_once('C:/xampp/htdocs/project/include/studentdetails.php');
+require_once('C:/xampp/htdocs/project/include/ay.php');
+require_once('C:/xampp/htdocs/project/include/exam.php');
+require_once('C:/xampp/htdocs/project/include/percent.php');
+require_once('C:/xampp/htdocs/project/include/grade.php');
 
-require_once(LIB_PATH.DS."database.php");
+require_once('C:/xampp/htdocs/project/include/database.php');
 ?>
 
 
